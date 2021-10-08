a6930036cce9edd6b9057faafed36c6a3762913d
James Tuong <jtuong@live.unc.edu>
2021-10-08 16:53:12 -0400

    testing mild

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 // Set options as a parameter, environment variable, or rc file.
 // eslint-disable-next-line no-global-assign
-require = require("esm")(module/* , options */)
-module.exports = require("./main.js")
+import esm from "esm"
 
+import { sumToString } from "./src/mild/mild_1.js"
+console.log(sumToString(13, 7));
