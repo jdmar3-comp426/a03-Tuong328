7bb6176b270d456cec05785aa252216f9538d281
James Tuong <jtuong@live.unc.edu>
2021-10-09 16:46:59 -0400

    mild2

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -69,10 +69,12 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-   var newObj = Object.assign(newObj, object);
+   var newObj = new Object();
+   for (const property in object) {
+      newObj[property] = object[property];
+   }
    delete newObj[key];
    return newObj;
-    
 }
 
 /**
